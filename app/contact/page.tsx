aaba50ffa01b4b0cde2ad587f6fc41b32c2aee02
whiteslam <gauravm7722@gmail.com>
2025-11-24 02:24:04 +0530

    remove extra footer

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,11 +1,9 @@
 import ContactUs from "../components/ContactUs";
-import Footer from "../components/Footer";
 
 export default function ContactPage() {
   return (
     <main>
       <ContactUs />
-      <Footer />
     </main>
   );
 }
